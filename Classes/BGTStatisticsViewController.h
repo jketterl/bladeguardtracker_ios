3f00771d7ff5ed3ece45f7aa767c274e90e739c3
Jakob Ketterl <jketterl@chipxonio.de>
2012-07-15 17:03:50 +0200

    adding navigation controller, statistics and map screens

---
+++ Classes/BGTStatisticsViewController.h
@@ -0,0 +1,13 @@
+//
+//  BGTStatisticsViewController.h
+//  BladeGuardTracker
+//
+//  Created by Jakob Ketterl on 15.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGTStatisticsViewController : UIViewController
+
+@end
---
+++ Classes/BGTStatisticsViewController.m
@@ -0,0 +1,43 @@
+//
+//  BGTStatisticsViewController.m
+//  BladeGuardTracker
+//
+//  Created by Jakob Ketterl on 15.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BGTStatisticsViewController.h"
+
+@interface BGTStatisticsViewController ()
+
+@end
+
+@implementation BGTStatisticsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return YES;
+}
+
+@end
